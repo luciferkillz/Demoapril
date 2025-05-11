b0252f395aa0ededd9f91dab64fdebd5ffc5869b
luciferkillz <salmankhan14786@gmail.com>
2025-05-11 12:39:01 +0530

    Updated with new text

--- demo-git/src/demo/GitDemo.java
+++ demo-git/src/demo/GitDemo.java
@@ -4,5 +4,6 @@ public class GitDemo {
 		public static void main(String[] args) {
 			System.out.println("Git demo");
 			System.out.println("Git is useful");
+			System.out.println("hello Git");
 		}
 }
